87bb265cdf11ae4e446fb791b09a73d3ad30db8f
Nataliia_Rud <carmagsales@gmail.com>
2023-03-13 10:18:36 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log(1);
+import fetchPictures from './js/API';
+const formEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+let page = 1;
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { searchQuery } = event.currentTarget.elements;
+  fetchPictures(searchQuery.value, 1).then(response =>
+    galleryRender(response.hits)
+  );
+
+  page++;
+}
+
+function galleryRender(hits) {
+  const markup = hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>${likes}/b>
+    </p>
+    <p class="info-item">
+      <b>${views}</b>
+    </p>
+    <p class="info-item">
+      <b>${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>${downloads}</b>
+    </p>
+  </div>
+</div>`
+    )
+    .join('');
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
+
+formEl.addEventListener('submit', onSubmit);
+
+//console.log(fetchPictures('cat', 1));
+
+//------------------------------------------
---
+++ src/js/API.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+const BASE_URL = 'https://pixabay.com/api/';
+const USER_KEY = '34317827-ccdab7647b739ba94e7a7be8e';
+const PARAMETERS = '&image_type=photo&orientation=horizontal&safesearch=true';
+const HITS_PER_PAGE = 40;
+
+export default function fetchPictures(query, page) {
+  return axios
+    .get(
+      `${BASE_URL}?key=${USER_KEY}&q=${query}${PARAMETERS}&per_page=${HITS_PER_PAGE}&page=${page}`
+    )
+    .then(({ data }) => data);
+  // return fetch(
+  //   `${BASE_URL}?key=${USER_KEY}&q=${query}${PARAMETERS}&per_page=${HITS_PER_PAGE}&page=${page}`
+  // )
+  //   .then(response => response.json())
+  //   .catch(error => console.log(error.name));
+}
+
+// .then(response => {
+//     if (response.ok) {
+//       console.log(1, response.json());
+//       return response.json();
+//     } else {
+//       console.log('error');
+//     }
+//   });
+//.catch(error => console.log(error.name));
