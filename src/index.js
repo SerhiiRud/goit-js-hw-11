ad337f142f46be1557a5ffada8a4b3871844dee2
Nataliia_Rud <carmagsales@gmail.com>
2023-03-16 17:26:43 +0200

    before async

--- src/index.js
+++ src/index.js
@@ -1,19 +1,86 @@
 import SimpleLightbox from 'simplelightbox';
+import Notiflix from 'notiflix';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import fetchPictures, { HITS_PER_PAGE } from './js/API';
 
-import fetchPictures from './js/API';
 const formEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+const buttonMore = document.querySelector('.load-more');
+
+buttonMore.style.display = 'none';
 let page = 1;
+let query = '';
 
 function onSubmit(event) {
   event.preventDefault();
   const { searchQuery } = event.currentTarget.elements;
-  fetchPictures(searchQuery.value, 1).then(response =>
-    galleryRender(response.hits)
-  );
+  query = searchQuery.value;
+  resetPage();
+  buttonMore.style.display = 'none';
+  fetchPictures(query, page)
+    .then(response => {
+      if (response.hits.length === 0) {
+        galleryEl.innerHTML = '';
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        event.target.reset();
+        return;
+      }
+      galleryEl.innerHTML = '';
+      Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
+      if (response.hits.length < HITS_PER_PAGE) {
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        buttonMore.style.display = 'none';
+        galleryRender(response.hits);
+        return;
+      }
+      galleryRender(response.hits);
+      buttonMore.style.display = 'inline-block';
+      incrementPage();
+    })
+    .then(page => setTimeout(autoScroll, 2000))
+    .catch(error => console.log(error.name));
+}
 
-  page++;
+function onLoadMore() {
+  fetchPictures(query, page)
+    .then(response => {
+      galleryRender(response.hits);
+      incrementPage();
+      if (
+        response.hits.length < HITS_PER_PAGE ||
+        response.hits.length * (page - 1) > response.totalHits
+      ) {
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        buttonMore.style.display = 'none';
+        return;
+      }
+    })
+    .then(page => autoScroll())
+    .catch(error => console.log(error.message));
+}
+
+function autoScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function incrementPage() {
+  page += 1;
+}
+
+function resetPage() {
+  page = 1;
 }
 
 function galleryRender(hits) {
@@ -48,6 +115,7 @@ function galleryRender(hits) {
     )
     .join('');
   galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
 
 const lightbox = new SimpleLightbox('.gallery a', {
@@ -58,4 +126,15 @@ const lightbox = new SimpleLightbox('.gallery a', {
   animationSpeed: 500,
 });
 
+const onButtonIntersect = entities => {
+  const [button] = entities;
+  if (button.isIntersecting) {
+    onLoadMore();
+  }
+};
+
+const observer = new IntersectionObserver(onButtonIntersect);
+observer.observe(buttonMore);
+
 formEl.addEventListener('submit', onSubmit);
+buttonMore.addEventListener('click', onLoadMore);
--- src/js/API.js
+++ src/js/API.js
@@ -3,8 +3,7 @@ import Notiflix from 'notiflix';
 const BASE_URL = 'https://pixabay.com/api/';
 const USER_KEY = '34317827-ccdab7647b739ba94e7a7be8e';
 const PARAMETERS = '&image_type=photo&orientation=horizontal&safesearch=true';
-const HITS_PER_PAGE = 40;
-
+export const HITS_PER_PAGE = 40;
 export default function fetchPictures(query, page) {
   return axios
     .get(
